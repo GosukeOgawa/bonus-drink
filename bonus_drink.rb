137820e545085b862094874eb2eb6d72d82c923b
SNP <ogawa.gosuke@gmail.com>
2017-02-21 17:52:46 +0900

    BonusDrinkクラスとそのテストを作成した。
    BonusDrinkクラスを実装。
    
    テストケースを作成。

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,21 @@
 class BonusDrink
+
   def self.total_count_for(amount)
-    0
+    total_count = amount;   # 飲んだ合計本数
+    empty_bottles = amount; # 空き瓶の数。増減する
+
+    # 空ボトルが3本未満になるまで繰り返す
+    while (empty_bottles > 2)
+
+      # おまけドリンクをもらう。空き瓶は消費される
+      bonus_drink = empty_bottles / 3;
+      empty_bottles = empty_bottles % 3;
+
+      # もらった分を総計と空き瓶数に追加
+      total_count += bonus_drink;
+      empty_bottles += bonus_drink;
+    end
+
+    return total_count;
   end
 end
